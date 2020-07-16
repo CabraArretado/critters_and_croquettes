80ea1f3f07039a5804092616e8056763ef20379b
felipetempus <felipemouradev@gmail.com>
2020-07-16 11:40:31 -0400

    Food done

--- animals.py
+++ animals.py
@@ -11,130 +11,146 @@ from datetime import date
 # self.swimming = True
 # self.slithering = True
 # self.walking = True
-
+# self.food = food
 
 class Llama:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
         self.shift = shift
-ted = Llama("Ted", "Llama", "afternoon")
+        self.food = food
+ted = Llama("Ted", "Llama", "afternoon", "Llama Food")
 
 class Wolf:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
         self.shift = shift
-joe = Wolf("Joe", "Wolf", "morning")
+        self.food = food
+joe = Wolf("Joe", "Wolf", "morning", "Wolf food")
 
 class Bear:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
         self.shift = shift
-xi = Bear("Xi", "Bear", "afternoon")
+        self.food = food
+xi = Bear("Xi", "Bear", "afternoon", "bear Food")
 
 class Kangooro:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
         self.shift = shift
-jumpie = Kangooro("Jumpie", "Kangooro", "afternoon")
+        self.food = food
+jumpie = Kangooro("Jumpie", "Kangooro", "afternoon", "Straw")
 
 class Pig:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.walking = True
         self.shift = shift
-frajola = Pig('Frajola', "Pig", "morning")
+        self.food = food
+frajola = Pig('Frajola', "Pig", "morning", "Pig Food")
 
 class Snake:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
-penelope = Snake("Penelope", "Snake")
+        self.food = food
+penelope = Snake("Penelope", "Snake", "Live Mouses")
 
 class Kobra:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
-jonathan = Kobra("Jonathan", "Kobra")
+        self.food = food
+jonathan = Kobra("Jonathan", "Kobra", "Live Mouses")
 
 
 class Salamander:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
-piupiu = Salamander("Piupiu", "Salamander")
+        self.food = food
+piupiu = Salamander("Piupiu", "Salamander", "Salamander Food")
 
 
 class Crocodile:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
-jeferson = Crocodile("Jeferson", "Crocodile")
+        self.food = food
+jeferson = Crocodile("Jeferson", "Crocodile", "Alive Sheep")
 
 class Aligator:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.slithering = True
-bob = Aligator("Bob", "Aligator")
+        self.food = food
+bob = Aligator("Bob", "Aligator", "Live Aligator")
 
 class Crabby:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.swimming = True
-krabby = Crabby("Krabby", "Crabby")
+        self.food = food
+krabby = Crabby("Krabby", "Crabby", "Crabby Food")
 
 class Seahorse:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.swimming = True
-rapidash = Seahorse("Rapidash", "Seahorse")
+        self.food = food
+rapidash = Seahorse("Rapidash", "Seahorse", "Seahorse Food")
 
 class Squid:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.swimming = True
-jujuba = Squid("Jujuba", "Squid")
+        self.food = food
+jujuba = Squid("Jujuba", "Squid", "squid Food")
 
 class Jellyfish:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.swimming = True
-pikachu =   Jellyfish("Pikachu", "Jellyfish")
+        self.food = food
+pikachu =   Jellyfish("Pikachu", "Jellyfish", "Jellyfish Food")
 
 class Fish:
-    def __init__(self, name, species):
+    def __init__(self, name, species, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
         self.swimming = True
+        self.food = food
+seaking = Fish("Seaking", "Fish", "Fish Food")
